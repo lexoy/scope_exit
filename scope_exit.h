f54cdbd2843b4c09be66a81cd84725deaa958e09
lexo <lexo_y@icloud.com>
2016-01-23 18:27:54 -0800

    Complete

--- scope_exit.h
+++ scope_exit.h
@@ -1,20 +1,38 @@
 #pragma once
 
-namespace _scope_exit_handler {
+namespace __scope_exit_handler {
 	template< typename T >
 	class scope_exit
 	{
 		public:
-			explicit scope_exit( T callable_exit ) : callable_exit( callable_exit ){}
-			~scope_exit(){try{callable_exit();}catch( ... ){}}
+			explicit scope_exit( T&& callable_exit ) : callable_exit( std::forward< T >( callable_exit ) ) { }
+			~scope_exit() { try{ callable_exit(); }catch( ... ){ } }
 
 		private:
 			T callable_exit;
+
+			// Disable operator assignment
+			const scope_exit& operator= ( const scope_exit& );
+			
 	};
 
 	template< typename T >
-	auto create_scope_exit( T callable_exit )
+	auto create_scope_exit( T&& callable_exit )->scope_exit< T >
 	{
-		return scope_exit< T >( callable_exit );
+		return scope_exit< T >( std::forward< T >( callable_exit ) );
 	}
 }
+
+#ifdef UTILITY_SCOPE_EXIT
+#undef UTILITY_SCOPE_EXIT
+#endif
+#ifdef _UTILITY_EXIT_SCOPE_LINENAME
+#undef _UTILITY_EXIT_SCOPE_LINENAME
+#endif
+#ifdef _UTILITY_EXIT_SCOPE_LINENAME_CAT
+#undef _UTILITY_EXIT_SCOPE_LINENAME_CAT
+#endif
+
+#define _UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line) name##line
+#define _UTILITY_EXIT_SCOPE_LINENAME(name, line) _UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line)
+#define UTILITY_SCOPE_EXIT(f) const auto& _UTILITY_EXIT_SCOPE_LINENAME(exit_handler, __LINE__) = __scope_exit_handler::create_scope_exit(f)
--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
 #include "scope_exit.h"
 
+using namespace std;
+
 int main() {
+	cout << "before UTILITY_SCOPE_EXIT" << endl;
+	UTILITY_SCOPE_EXIT( [=]{cout << "Inner Lambda 01" << endl;} );
+	UTILITY_SCOPE_EXIT( [=]{cout << "Inner Lambda 02" << endl;} );
+	cout << "after UTILITY_SCOPE_EXIT" << endl;
 
 	return 0;
 }
